1bb0866be09f0c48a16f44ba546357b7300acd9f
Moryoka <mariovlaviano2005@yahoo.com>
2023-10-13 12:53:28 +0300

    customize 404

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h2 className="font-medium text-3xl">404 <span className="font-thin">|</span> Not found</h2>
+    </div>
+  )
+}
+
+export default NotFound
