ac451ee0d96bb17d4e05f849e94246bcb615c46b
Moryoka <mariovlaviano2005@yahoo.com>
2023-11-02 13:00:16 +0200

    remove canonical link

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,6 @@ export default function Home() {
       <NextSeo
         title="Freestyle Barber EF | Salon frizerie Brăila"
         description="Un barber shop diferit. Pentru 'el' și pentru 'ea'. Experiență de 5 stele la cele mai mici prețuri din oraș."
-        canonical="https://freestylebarber-ef.ro/"
         openGraph={{
           type: 'website',
           locale: 'ro-RO',
